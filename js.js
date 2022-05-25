a3c85e0d0f2766fde5adac2b1fc50ad3944307f9
odc <odc@iMac-de-odc-7.local>
2022-05-25 10:51:13 +0100

    added the file jenkins

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
 console.log("hellos")
+let x = 0;
+while(true){
+    let len = 5*Math.sin(x)
+    let st = "";
+    for(let i=0;i<len;i++){
+        st += "a";
+    }
+    console.log(st);
+    x++;
+}
